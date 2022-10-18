be5f9b11046873a1779f85d3aa5af77a7dff93b8
ajomuch92 <aaronmontes_92@yahoo.es>
2022-10-18 17:39:47 -0600

    Initial commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿int miContrasenia = 11249200;
+Console.WriteLine("Ingresa la contraseña");
+int contraseniaIngresada = 0;
+
+if (int.Parse(Console.ReadLine()) == miContrasenia)
+{
+    Console.WriteLine("Bienvenid@");
+}
+else
+{
+    Console.WriteLine("Ingresa la contraseña nuevamente");
+    contraseniaIngresada = int.Parse(Console.ReadLine());
+    if (contraseniaIngresada == miContrasenia)
+    {
+        Console.WriteLine("Bienvenid@");
+    }
+    else
+    {
+        Console.WriteLine("Reiniciar el sistema");
+    }
+}
