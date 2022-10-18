ff10971b1f55508711f281151d8b866a844b3a6a
ajomuch92 <aaronmontes_92@yahoo.es>
2022-10-18 17:59:04 -0600

    feat: 🎸 Agregando proyecto para decisiones

---
+++ decisiones/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Ingresa tu inventario:");
+
+int inventario = int.Parse(Console.ReadLine());
+
+if (inventario < 10)
+{
+    Console.WriteLine("Debes pedir más productos");
+    Console.WriteLine("¿Cuántos productos deseas agregar?");
+    int productosASumar = int.Parse(Console.ReadLine());
+    inventario += productosASumar;
+    if (inventario > 100)
+    {
+        Console.WriteLine("Has pedido demasiado");
+    }
+}
+else if (inventario > 100)
+{
+    Console.WriteLine("Has pedido demasiado");
+}
+else
+{
+    Console.WriteLine("Estamos bien :)");
+}
