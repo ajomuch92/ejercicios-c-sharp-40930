eb26a6a4365c1fbeaa19c9c4c4995a96644e7426
ajomuch92 <aaronmontes_92@yahoo.es>
2022-10-18 17:53:51 -0600

    feat: 🎸 Agregando proyecto para las varialbes

---
+++ Variables/Program.cs
@@ -0,0 +1,24 @@
+﻿int entero = 0;
+
+float floatante = 3f;
+
+string cadena = "Esta es una cadena";
+
+char caracter = 'a';
+
+double numeroDecimal = 4.5;
+
+bool booleano = true;
+
+bool decision = entero == 0;
+
+Console.WriteLine("Ingrese tu nombre:");
+string miNombre = Console.ReadLine();
+
+Console.WriteLine("Ingrese un edad (años): ");
+short edad = short.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingresa tu altura(en mts):");
+float altura = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Bienvenido {miNombre}, tienes {edad} años y mides {altura} mts");
