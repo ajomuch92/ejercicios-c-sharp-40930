c97b048d0956ca3fcc063a6e122fb80c5d98a783
ajomuch92 <aaronmontes_92@yahoo.es>
2022-10-18 18:05:07 -0600

    feat: 🎸 Agregando proyecto de conversión de temperatura

---
+++ ConversorTemperaturas/Program.cs
@@ -0,0 +1,24 @@
+﻿int opcion;
+double resultado = 0, temperatura = 0;
+Console.Write("Ingresa el valor de temperatura: ");
+temperatura = double.Parse(Console.ReadLine());
+Console.WriteLine("Selecciona el valor de opcion.");
+Console.WriteLine("\t1.- Fahrenheit a Celsius");
+Console.WriteLine("\t2.- Celsius a Fahrenheit");
+Console.Write("\t: ");
+opcion = int.Parse(Console.ReadLine());
+
+if (opcion == 1)
+{
+    resultado = (temperatura - 32) / 1.8;
+}
+else if (opcion == 2)
+{
+    resultado = 1.8 * temperatura + 32;
+}
+else
+{
+    Console.WriteLine("Has ingresado una opción no válida");
+    return;
+}
+Console.WriteLine("Resultado de la conversión: " + resultado);
