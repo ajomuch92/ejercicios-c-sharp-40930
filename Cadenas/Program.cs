b339eae415ba18954a39ef8c0d24f672c630d75d
ajomuch92 <aaronmontes_92@yahoo.es>
2022-10-23 10:56:46 -0600

    feat: 🎸 Agregando ejemplo para ciclos while

---
+++ Cadenas/Program.cs
@@ -0,0 +1,43 @@
+﻿// Cadenas
+string mensaje = "Hola Mundo";
+
+Console.WriteLine("Cadena original " + mensaje);
+
+Console.WriteLine("Longitud " + mensaje.Length);
+
+Console.WriteLine("Mayúscula " + mensaje.ToUpper());
+
+Console.WriteLine("Minúscula " + mensaje.ToLower());
+
+Console.WriteLine(mensaje.Equals("Hola"));
+
+Console.WriteLine(mensaje.Contains("hola"));
+
+Console.WriteLine("Primera letra " + mensaje[0]);
+
+string vacio = ""; // string.Empty
+string? nulo = null;
+
+if (string.IsNullOrEmpty(vacio)) // if (vacio == "" || vacio == string.Empty || vacio == null)
+{
+    Console.WriteLine("Mi cadena está vacía");
+}
+
+if (vacio == nulo)
+{
+    Console.WriteLine("Mis cadenas son iguales");
+}
+else
+{
+    Console.WriteLine("Mis cadenas son distintas");
+}
+
+int numero = 1;
+float flotante = 3.15f;
+bool decision = true;
+
+Console.WriteLine(numero.ToString());
+Console.WriteLine(flotante.ToString());
+Console.WriteLine(decision.ToString());
+
+Console.WriteLine(Convert.ToBoolean("True"));
---
+++ CiclosWhile/Program.cs
@@ -0,0 +1,25 @@
+﻿// Ciclos While
+
+// while
+int edad = 0;
+
+while(true)
+{
+    Console.WriteLine("Dame tu edad");
+    edad = int.Parse(Console.ReadLine());
+    if (edad == 0)
+    {
+        break;
+    }
+}
+
+
+// do while
+
+double altura = 0;
+
+do
+{
+    Console.WriteLine("Dame tu altura en mt");
+    altura = double.Parse(Console.ReadLine());
+} while (altura != 0);
