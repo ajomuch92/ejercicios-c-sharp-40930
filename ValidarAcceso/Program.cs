2e9559818b9a3556edb88e5adf242a7b606f9b9a
ajomuch92 <aaronmontes_92@yahoo.es>
2022-10-23 11:16:32 -0600

    feat: 🎸 Agregando ejemplo para Validar acceso

---
+++ ValidarAcceso/Program.cs
@@ -0,0 +1,19 @@
+﻿const string contraseniaCorrecta = "usSystem-2022";
+const short CANTIDAD_MAX_INTENTOS = 5;
+short intentos = 0;
+
+do
+{
+    Console.WriteLine("Ingresa la contraseña");
+    string contraseniaIngresada = Console.ReadLine();
+    if (contraseniaIngresada == contraseniaCorrecta)
+    {
+        Console.WriteLine("Bienvenido al sistema");
+        break;
+    }
+    else
+    {
+        intentos++;
+        Console.WriteLine($"Contraseña incorrecta, tienes {CANTIDAD_MAX_INTENTOS - intentos} intentos");
+    }
+} while (intentos < CANTIDAD_MAX_INTENTOS);
