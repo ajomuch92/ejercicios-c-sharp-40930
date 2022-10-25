63fc4e7fcd5073c799c254246f2762a1655cd8b8
ajomuch92 <aaronmontes_92@yahoo.es>
2022-10-24 18:23:24 -0600

    feat: 🎸 Creando proyecto para ejemplo de funciones

---
+++ funciones/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("------------");
+saludar("Lucas");
+saludar("Pablo");
+Console.WriteLine("****************");
+
+double numero1 = double.Parse(Console.ReadLine());
+double numero2 = double.Parse(Console.ReadLine());
+
+double resultado = sumaNumeros(numero1, numero2);
+
+Console.WriteLine("La suma es " + resultado);
+
+void saludar(String nombre)
+{
+    Console.WriteLine("Hola " + nombre); // 'a' "asdfasdf"
+}
+
+double sumaNumeros(double numero1, double numero2)
+{
+    return numero1 + numero2;
+}
