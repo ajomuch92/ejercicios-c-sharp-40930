43f1deb66633adbfa4cfc9cba3a58ddbf260db91
Aarón J. Montes <aaronmontes_92@yahoo.es>
2022-11-21 16:51:41 -0600

    feat: 🎸 Agregando ejemplos para listas y diccionarios

---
+++ Listas/Personas.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Listas
+{
+    public class MiClase
+    {
+
+    }
+
+    public class Persona
+    {
+        public string nombre { get; set; }
+        // Composición
+        public MiClase miClase { get; set; }
+
+        public Persona()
+        {
+            nombre = string.Empty;
+        }
+
+        public Persona(string nombre)
+        {
+            this.nombre = nombre;
+        }
+    }
+    public class Personas
+    {
+        private List<Persona> personas = new List<Persona>();
+
+        public Personas() { }
+
+        public Personas(List<Persona> personas)
+        {
+            this.personas = personas;
+        }
+
+        public Personas(Persona[] personas)
+        {
+            foreach(Persona persona in personas)
+            {
+                this.personas.Add(persona);
+            }
+        }
+
+        public void insertarPersona(Persona persona)
+        {
+            personas.Add(persona);
+        }
+
+        public void removerPersona(Persona persona)
+        {
+            if (persona == null)
+            {
+                throw new Exception("Se requiere un valor válido para agregar una persona");
+            }
+            else if (personas.Contains(persona))
+            {
+                personas.Remove(persona);
+            }
+            else
+            {
+                throw new Exception("Persona no encontrada");
+            }
+        }
+    }
+}
---
+++ Listas/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Listas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*List<string> nombres = new List<string> { "Lucas", "Matias", "Adrian"};
+            Console.WriteLine($"Capacidad de la lista {nombres.Capacity}");
+            Console.WriteLine("Escribe tu nombre");
+            string miNombre = Console.ReadLine();
+            Console.WriteLine("Antes de agregar");
+            Console.WriteLine(nombres.Count);
+            nombres.Add(miNombre);
+            Console.WriteLine("Después de agregar");
+            Console.WriteLine(nombres.Count);
+            Console.WriteLine("Recorriendo la lista");
+            foreach(string nombre in nombres)
+            {
+                Console.WriteLine("Hola {0} :)", nombre);
+            }
+            Console.WriteLine("Insertando");
+            nombres.Insert(1, "Camila");
+            Console.WriteLine(nombres.Count);
+            Console.WriteLine("Removiendo");
+            nombres.Remove("Adrian");
+            Console.WriteLine($"Capacidad de la lista {nombres.Capacity}");
+            Console.WriteLine(nombres.Count);
+            bool contieneNombreLucas = nombres.Contains("Lucas");
+            Console.WriteLine(contieneNombreLucas);
+            int indiceMatias = nombres.IndexOf("Matias");
+            Console.WriteLine(indiceMatias);
+            nombres.Sort();
+            foreach (string nombre in nombres)
+            {
+                Console.WriteLine("{0} :)", nombre);
+            }
+            Console.WriteLine("Limpiando lista");
+            nombres.Clear();
+            Console.WriteLine(nombres.Count);*/
+
+            int[,] numeros = new int[3, 3] { {1, 2, 3 }, { 4, 5, 6}, { 7, 8, 9 } };
+
+            for (int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine($"i: {i} j:{j} valor {numeros[i, j]}");
+                }
+            }
+        }
+    }
+}
